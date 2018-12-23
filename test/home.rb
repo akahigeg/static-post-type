e1444d502db061500e8d54e817716fccc144921b
akahigeg <akahigeg@gmail.com>
2018-12-23 15:28:03 +0900

    [test] Exec wp core install in CI

--- test/home.rb
+++ test/home.rb
@@ -1,15 +1,34 @@
 require "rubygems"
 require "test/unit"
-require 'selenium-webdriver'
+require "selenium-webdriver"
 
 class HomeTest < Test::Unit::TestCase
+  ADMIN_USER_NAME = 'admin@example.com'
+  ADMIN_USER_PASS = 'pass' 
+
   def setup
     @driver = Selenium::WebDriver.for :chrome
+    @wait = Selenium::WebDriver::Wait.new(timeout: 10)
   end
 
-  def test_home
-    @driver.get "http://localhost:8888"
-    assert_equal("localhost", @driver.title)
+  # def test_home
+  #   @driver.get "http://localhost:8888/"
+  #   assert_equal("localhost", @driver.title)
+  # end
+
+  def input(name, value)
+    @driver.find_element(:name, name).send_keys(value)
+
+  end
+
+  def test_login
+    @driver.get "http://localhost:8888/wp-admin/"
+    input("log", ADMIN_USER_NAME)
+    input("pwd", ADMIN_USER_PASS)
+    @driver.find_element(:id, "wp-submit").click
+
+    @wait.until { @driver.find_element(tag_name: "h1").text == "Dashboard" }
+    assert_equal("Dashboard ‹ StaticPostTypeDev — WordPress", @driver.title)
   end
 
   # def test_click
@@ -18,5 +37,3 @@ class HomeTest < Test::Unit::TestCase
   #   assert_equal("VOTE", @driver.title)
   # end
 end
-
-
