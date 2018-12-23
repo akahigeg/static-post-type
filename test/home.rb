600639c534e7bad3b649deaeee9ec4e79e500c0d
akahigeg <akahigeg@gmail.com>
2018-12-23 11:50:21 +0900

    Add Selenium and first step of test with Ruby.

---
+++ test/home.rb
@@ -0,0 +1,21 @@
+require "test/unit"
+require 'selenium-webdriver'
+
+class HomeTest < Test::Unit::TestCase
+  def setup
+    @driver = Selenium::WebDriver.for :chrome
+  end
+
+  def test_home
+    @driver.get "http://localhost:8888"
+    assert_equal("StaticPostTypeDev – Just another WordPress site", @driver.title)
+  end
+
+  # def test_click
+  #   @driver.get "http://localhost:8888"
+  #   @driver.find_element(:id, 'start-button').click
+  #   assert_equal("VOTE", @driver.title)
+  # end
+end
+
+
