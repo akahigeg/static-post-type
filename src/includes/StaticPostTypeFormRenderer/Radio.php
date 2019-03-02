2d81710e3b6c34ddfdf795731cd2fd9918ac9052
akahigeg <akahigeg@gmail.com>
2019-03-02 13:35:27 +0900

    Update comments for the checkbox renderer and radio renaderer.

--- src/includes/StaticPostTypeFormRenderer/Radio.php
+++ src/includes/StaticPostTypeFormRenderer/Radio.php
@@ -1,9 +1,44 @@
 <?php
+/**
+ * Class for rendering radios.
+ *
+ * @see StaticPostTypeFormRendererLabel
+ * @see StaticPostTypeFormRendererDescription
+ *
+ */
 class StaticPostTypeFormRendererRadio {
+    /**
+     * Render method for radios.
+     *
+     * @param string $field_name
+     * @param string $saved_value
+     * @param array $options
+     * @return void
+     */
   public static function render($field_name, $saved_value, $options) {
     echo self::build($field_name, $saved_value, $options);
   }
 
+    /**
+     * Build HTML for radios with a label and a description.
+     * Rendering `checked` by $saved_value
+     *
+     * @param string $field_name
+     * @param string $saved_value
+     * @param array $options
+     * @return string $html
+     *
+     * $options
+     * - values: Values of radios or values and labels of radios.
+     *     ex: $options['values'] = array('apple', 'orange', 'banana');
+     *     ex: $options['values'] = array('r' => 'Red', 'g' => 'Green', 'b' => 'Blue');
+     * - label: Label text.
+     * - label_class: CSS class attribute for the label.
+     * - label_style: Style attribute for the label.
+     * - description: Description displayed below the text field.
+     * - description_class: CSS class attribute for the description.
+     * - description_style: Style attribute for the description.
+     */
   public static function build($field_name, $saved_value, $options) {
     $html = StaticPostTypeFormRendererLabel::build($field_name, $options);
 
